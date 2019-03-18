e6fe888c4fad0411384dee96a757cd34f6eb247d
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2019-03-18 11:10:03 +0800

    add jpa test

--- 05-data/src/test/java/com/example/data/ApplicationTests.java
+++ 05-data/src/test/java/com/example/data/ApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.data;
 
 import com.example.data.druid.bean.Person;
 import com.example.data.druid.service.PersonService;
+import com.example.data.jpa.bean.User;
 import com.example.data.jpa.dao.IUserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -33,7 +39,29 @@ public class ApplicationTests {
 //        user2.setUserName("赵子龙");
 //        userRepository.save(user2);
 //        System.out.println(user2);
-        System.out.println(userRepository.findUserByUserNameAndAge("赵子龙", 6));
+//        System.out.println(userRepository.findUserByUserNameAndAge("赵子龙", 6));
+        //批量添加
+//        User user = new User();
+//        user.setId(3);
+//        user.setUserName("wcs2");
+//        user.setAge(32);
+//        User user2 = new User();
+//        user2.setId(4);
+//        user2.setUserName("wcs3");
+//        user2.setAge(43);
+//        List<User> userList = new ArrayList<>();
+//        userList.add(user);
+//        userList.add(user2);
+//        userRepository.saveAll(userList);
+        //分页
+        Pageable pageable = PageRequest.of(1, 2);
+        Page<User> userPage = userRepository.findAll(pageable);
+        System.out.println(userPage.getTotalElements());
+        System.out.println(userPage.getTotalPages());
+        List<User> userList = userPage.getContent();
+        for (User user : userList) {
+            System.out.println(user);
+        }
     }
 
 }
